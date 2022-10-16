742adeca33b25abde89c66570c9e220b518bb937
truonglpn <nhatruong.lephung@gmail.com>
2022-10-16 15:28:32 +0700

    warehouse add nuget material design theme

---
+++ WareseHouse/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WareseHouse.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            MessageBox.Show("here");
+        }
+    }
+}
